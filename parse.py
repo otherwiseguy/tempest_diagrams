0d69f80140ef527945f00f7c0e4195bdcd3b32e3
Terry Wilson <twilson@redhat.com>
2019-09-27 07:58:02 -0500

    Add time note

--- parse.py
+++ parse.py
@@ -21,7 +21,7 @@ FUNC = r'\((?P<function>[^)]+)\):'
 STATUS = r'(?P<status>\d{3})'
 METHOD = r'(?P<method>[A-Z]+)'
 URL = r'(?P<url>\S+)'
-SP_INFO_TAIL = r'\s*(.*)'
+SP_INFO_TAIL = r'\s*(?P<tail>.*)'
 SP = r'\s+'
 DEBUG_REQ = r'Request - Headers:\s+(?P<req_headers>.*)\n\s*Body: (?P<req_body>.*)\n'
 DEBUG_RESP =r'\s*Response - Headers: (?P<resp_headers>.*)\n\s*Body: (?P<resp_body>.*?)(?:_log_request_full.*)?'
@@ -83,11 +83,12 @@ class TempestResponse(object):
 
 
 class TempestLogItem(object):
-    def __init__(self, req_id, function, req=None, resp=None):
+    def __init__(self, req_id, function, req=None, resp=None, time=None):
         self.req_id = req_id
         self.function = function
         self.request = req
         self.response = resp
+        self.time = time
 
     @classmethod
     def from_lines(cls, req_id, info, debug):
@@ -116,14 +117,16 @@ class TempestLogItem(object):
         response = TempestResponse(status=info_match.groupdict()['status'],
                                    headers=ast.literal_eval(debug_match.groupdict()['resp_headers']),
                                    body=resp_body)
-        item = cls(req_id, function, request, response)
+        time = info_match.groupdict()['tail']
+        item = cls(req_id, function, request, response, time)
         return item
 
     def __str__(self):
         return "{}({}) {} {} {}".format(self.req_id, self.function, self.response.status, self.request.method, self.request.url.path)
 
     def to_diagram(self):
-        return "Client->{host}: {method} {path}\n{host}->Client: {status}".format(method=self.request.method, host=self.request.target, path=self.request.url.path, status=self.response.status)
+        note = '\nNote right of {host}: {time}'.format(host=self.request.target, time=self.time) if self.time else ''
+        return "Client->{host}: {method} {path}{note}\n{host}->Client: {status}".format(method=self.request.method, host=self.request.target, path=self.request.url.path, status=self.response.status, note=note)
 
 
 def messages(f):
